49250586a0777361e847a081b3af3a335cb6d627
Marving14 <bryan_marving@hotmail.com>
2019-11-20 17:38:14 -0600

    console log

--- public/js/home.js
+++ public/js/home.js
@@ -72,8 +72,8 @@ function watchForm(){
 	
 	///////////////////////////////////////////////////////////////////////
 	
-    if( emailPerson.value !=""){
-		//  && skillsPerson.value && degreePerson.value && birthdayPerson.value && agePerson.value && fullNamePerson.value
+    if( emailPerson.value !="" && birthdayPerson.value != ""){
+		//  && skillsPerson.value && degreePerson.value &&  && agePerson.value && fullNamePerson.value
 			console.log("A"); 
 	let postD = $(".formElement");
 	console.log(postD); 
